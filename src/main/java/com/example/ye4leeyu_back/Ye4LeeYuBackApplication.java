9a7c793e9c1358cd960c0b2c6180fe5ec19292fc
uojin29 <uojin29@gmail.com>
2024-07-22 23:17:05 +0900

    gitinit

---
+++ src/main/java/com/example/ye4leeyu_back/Ye4LeeYuBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ye4leeyu_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ye4LeeYuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ye4LeeYuBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ye4leeyu_back/Ye4LeeYuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ye4leeyu_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ye4LeeYuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
