f4f980305c3c339e4a14e98b996a06fe82c84064
uojin29 <uojin29@gmail.com>
2024-07-30 20:19:38 +0900

    feat : 찜한 강좌 보여주기 완료 (#19)

--- src/main/java/com/example/ye4leeyu_back/presentation/controller/MyPageController.java
+++ src/main/java/com/example/ye4leeyu_back/presentation/controller/MyPageController.java
@@ -1,6 +1,7 @@
 package com.example.ye4leeyu_back.presentation.controller;
 
 import com.example.ye4leeyu_back.application.usecase.HistoryUseCase;
+import com.example.ye4leeyu_back.presentation.response.CourseResponse;
 import com.example.ye4leeyu_back.presentation.response.HistoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class MyPageController {
     private final HistoryUseCase historyUseCase;
 
+
     @GetMapping("/mypage/exp")
     public ResponseEntity<HistoryResponse> getHistory() {
         return ResponseEntity.ok(historyUseCase.getHistory());
+    }
+
+    @GetMapping("/mypage/like")
+    public ResponseEntity<List<CourseResponse>> getLikeCourse() {
+        return ResponseEntity.ok(historyUseCase.getLikeCourse());
     }
 }
